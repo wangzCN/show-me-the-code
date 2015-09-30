19f593c21a7e54f66e473687d52eca9f11ce082c
wangzCN <wangz.cn@outlook.com>
2015-09-30 15:06:46 +0800

    solution of 0002 added

--- 0002/src/0002.py
+++ 0002/src/0002.py
@@ -1,10 +1,13 @@
+#Windows 10
+#python 2.7.10
+#eclipse with pydev
+#redis 2.8 for windows
 '''
 
 @author: Wangz.cn
 '''
 import random
-import MySqlCon
-import MySQLdb 
+import redis
 
 def newKey(length = 16):
     chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
@@ -14,15 +17,10 @@ def newKey(length = 16):
         str += chars[random.randint(0, str_len)]
     return str
 
-# link to Mysql
-con = MySqlCon.Con()
+red_con = redis.Redis(host="localhost",port = 6379 ,db = 0)
 
 for i in range(200):
-    
-    key = newKey()
-    print(key)
-    
-    #insert to mysql
-    MySqlCon.Insert(con, key)
-    
-MySqlCon.done(con)
+    activekey = newKey()
+    key_id = i;
+    print(activekey)
+    red_con.set(key_id,activekey)
